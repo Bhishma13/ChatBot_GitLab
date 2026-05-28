ea873fc85c8719955ca1d358beb7b17ff9465cef
Bhishma13 <bhishmapadhayay@gmail.com>
2026-05-28 22:45:02 +0530

    remove KeepAliveService since cron-job.org is handling pings

--- backend/src/main/java/com/gitlab/chatbot/ChatbotApplication.java
+++ backend/src/main/java/com/gitlab/chatbot/ChatbotApplication.java
@@ -3,10 +3,7 @@ package com.gitlab.chatbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.scheduling.annotation.EnableScheduling;
-
 @SpringBootApplication
-@EnableScheduling
 public class ChatbotApplication {
 
 	public static void main(String[] args) {
